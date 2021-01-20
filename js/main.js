040b39e052a01762a618b774e337e9dd741553ab
Anatoluch <crimeagold@mail.ru>
2021-01-20 14:17:34 +0300

    1th commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function(){
+   $(".owl-carousel").owlCarousel({
+      items: 1,
+      nav: true,
+      navSpeed: 800,
+      dots: false,
+      navText: ['', ''],
+      loop: true
+   });
+ });
